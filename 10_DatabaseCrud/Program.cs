3e85b0cb41d9207aab3caf679a1be74dd7d48ef8
Arif SARI <arif.d.sari@gmail.com>
2024-10-28 00:42:54 +0300

    Database işlemleri ekleme

--- 10_DatabaseCrud/Program.cs
+++ 10_DatabaseCrud/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -16,15 +17,92 @@ namespace _10_DatabaseCrud
             Console.WriteLine();
 
             Console.WriteLine("----------------------------------");
-            Console.Write("Eklemek istediğiniz Kategori Adı: ");
-            string categoryName = Console.ReadLine();
+            #region Kategori Ekleme İşlemi
+            //Console.Write("Eklemek istediğiniz Kategori Adı: ");
+            //string categoryName = Console.ReadLine();
 
-            SqlConnection connection = new SqlConnection("Data Source= MONSTER\\TEW_SQLEXPRESS; " +
-                "initial catalog=EgitimKampiDb; integrated security=truer " );
-            connection.Open();
+            //SqlConnection connection = new SqlConnection("Data Source= MONSTER\\TEW_SQLEXPRESS; " +
+            //    "initial catalog=EgitimKampiDb; integrated security=true " );
+            //connection.Open();
 
-             
+            ////sorgu yapma
+            //SqlCommand command = new SqlCommand("insert into TblCategory(CategoryName) values(@p1)",connection);
+            //command.Parameters.AddWithValue("@p1", categoryName);
+            //command.ExecuteNonQuery();
+            ////ekleme sırasındaki hiyerarşik yapı
+            //// (productName,price,stock) => values(@p1,@p2,@p3)
+            //// @SQLde atama yapmak için özel bir karakter
+            ////parantez içindeki sıralamaya göre karşılık bulur
 
+            //connection.Close();
+
+            //Console.WriteLine("Başarı ile eklendi");
+            #endregion
+
+            #region Ürün Ekleme İşlemi
+
+            //string productName;
+            //decimal procutPrice;
+            ////bool productStatus;
+
+            //Console.Write("Ürün adı : ");
+            //productName =Console.ReadLine();
+
+            //Console.Write("Ürün fiyat : ");
+            //procutPrice= decimal.Parse(Console.ReadLine());
+
+            //SqlConnection connection = new SqlConnection("Data source = MONSTER\\TEW_SQLEXPRESS; " +
+            //    "initial catalog=EgitimKampiDb; integrated security=true");
+
+            //connection.Open();
+
+            //SqlCommand command = new SqlCommand("insert into TblProduct(ProductName,ProductPrice,ProductStatus) " +
+            //    "values(@ProductName,@ProductPrice,@ProductStatus)",connection);
+            //command.Parameters.AddWithValue("@productName",productName);
+            //command.Parameters.AddWithValue("@productPrice",procutPrice);
+            //command.Parameters.AddWithValue("productStatus",true);
+
+            //command.ExecuteNonQuery();
+
+            //connection.Close();
+
+            //Console.WriteLine("Ürün başarı ile eklendi");
+
+
+
+
+            #endregion
+
+            #region Ürün Listeleme İşlemi
+
+            //SqlConnection connection = new SqlConnection("Data Source= MONSTER\\TEW_SQLEXPRESS;" +
+            //    " initial Catalog = EgitimKampiDb; integrated security=true ");
+
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("Select*From TblProduct", connection);
+            //SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+            ////dataları içeri alabilmek için DataTable
+            //DataTable dataTable = new DataTable();
+            ////içini doldurma
+            //adapter.Fill(dataTable);
+
+            //foreach (DataRow row in dataTable.Rows)
+            //{
+            //    foreach (var item in row.ItemArray)
+            //    {
+            //        Console.Write(item.ToString() + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //connection.Close();
+
+            #endregion
+
+            #region Ürün Silme İşlemi
+
+            #endregion 
+            Console.Read();
         }
     }
 }
