47f15401f7507fd9f1112b04f49a1413a1cbaf1c
Arif SARI <arif.d.sari@gmail.com>
2024-10-27 22:49:08 +0300

    Update

---
+++ 10_DatabaseCrud/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_DatabaseCrud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crud--> Create-Read-Update-Delete
+            Console.WriteLine("***** Menü Sipariş İşlem Paneli ***** ");
+            Console.WriteLine();
+
+            Console.WriteLine("----------------------------------");
+            Console.Write("Eklemek istediğiniz Kategori Adı: ");
+            string categoryName = Console.ReadLine();
+
+            SqlConnection connection = new SqlConnection("Data Source= MONSTER\\TEW_SQLEXPRESS; " +
+                "initial catalog=EgitimKampiDb; integrated security=truer " );
+            connection.Open();
+
+             
+
+        }
+    }
+}
