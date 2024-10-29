11a09c7d172862ef5d1f596c29370e82db09af01
Arif SARI <arif.d.sari@gmail.com>
2024-10-29 12:17:45 +0300

    10.ders sonu

--- 10_DatabaseCrud/Program.cs
+++ 10_DatabaseCrud/Program.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _10_DatabaseCrud
@@ -12,17 +13,18 @@ namespace _10_DatabaseCrud
     {
         static void Main(string[] args)
         {
-            //Crud--> Create-Read-Update-Delete
-            Console.WriteLine("***** Menü Sipariş İşlem Paneli ***** ");
-            Console.WriteLine();
 
-            Console.WriteLine("----------------------------------");
-            #region Kategori Ekleme İşlemi
+            #region CRUD
+            //Crud--> Create-Read-Update-Delete            //Console.WriteLine("***** Menü Sipariş İşlem Paneli ***** ");
+            //Console.WriteLine();
+
+            //Console.WriteLine("----------------------------------");
+            //#region Kategori Ekleme İşlemi
+
             //Console.Write("Eklemek istediğiniz Kategori Adı: ");
             //string categoryName = Console.ReadLine();
 
-            //SqlConnection connection = new SqlConnection("Data Source= MONSTER\\TEW_SQLEXPRESS; " +
-            //    "initial catalog=EgitimKampiDb; integrated security=true " );
+            //SqlConnection connection = new SqlConnection("Data Source= MONSTER\\TEW_SQLEXPRESS; initial catalog=EgitimKampiDb; integrated security=true " );
             //connection.Open();
 
             ////sorgu yapma
@@ -39,6 +41,7 @@ namespace _10_DatabaseCrud
             //Console.WriteLine("Başarı ile eklendi");
             #endregion
 
+
             #region Ürün Ekleme İşlemi
 
             //string productName;
@@ -51,13 +54,12 @@ namespace _10_DatabaseCrud
             //Console.Write("Ürün fiyat : ");
             //procutPrice= decimal.Parse(Console.ReadLine());
 
-            //SqlConnection connection = new SqlConnection("Data source = MONSTER\\TEW_SQLEXPRESS; " +
-            //    "initial catalog=EgitimKampiDb; integrated security=true");
+            //SqlConnection connection = new SqlConnection("Data source = MONSTER\\TEW_SQLEXPRESS; initial catalog=EgitimKampiDb; integrated security=true");
 
             //connection.Open();
 
             //SqlCommand command = new SqlCommand("insert into TblProduct(ProductName,ProductPrice,ProductStatus) " +
-            //    "values(@ProductName,@ProductPrice,@ProductStatus)",connection);
+            //"values(@ProductName,@ProductPrice,@ProductStatus)",connection);
             //command.Parameters.AddWithValue("@productName",productName);
             //command.Parameters.AddWithValue("@productPrice",procutPrice);
             //command.Parameters.AddWithValue("productStatus",true);
@@ -75,8 +77,7 @@ namespace _10_DatabaseCrud
 
             #region Ürün Listeleme İşlemi
 
-            //SqlConnection connection = new SqlConnection("Data Source= MONSTER\\TEW_SQLEXPRESS;" +
-            //    " initial Catalog = EgitimKampiDb; integrated security=true ");
+            //SqlConnection connection = new SqlConnection("Data Source= MONSTER\\TEW_SQLEXPRESS;initial Catalog = EgitimKampiDb; integrated security=true ");
 
             //connection.Open();
             //SqlCommand command = new SqlCommand("Select*From TblProduct", connection);
@@ -101,8 +102,60 @@ namespace _10_DatabaseCrud
 
             #region Ürün Silme İşlemi
 
-            #endregion 
+
+
+            //Console.Write("Silinecek ürün Id : ");  //önce kullanıcıya bir mesaj gönderelim
+            //int productId = int.Parse(Console.ReadLine());  //göndereceğimiz Id tanımlaması
+
+            ////Database tanımlama 
+            //SqlConnection connection = new SqlConnection("Data Source= MONSTER\\TEW_SQLEXPRESS; initial catalog=EgitimKampiDb ; integrated security=true");
+            //connection.Open();
+
+            ////yeni command ile sorgu yapma
+            //SqlCommand command = new SqlCommand("Delete From TblProduct Where ProductId =@productId",connection);
+            ////
+            //command.Parameters.AddWithValue("@productId",productId); // product Id değeri nereden geliyor= productId'den
+            //command.ExecuteNonQuery();  // değişiklikleri veri tabanına yansıtmalıyız
+
+            //connection.Close();
+            ////işlemi sonucunda bir mesaj göstermeliyiz
+
+            //Console.WriteLine("Silme işlemi yapıldı");
+            //#endregion 
+            #endregion
+
+            #region Ürün Güncelleme
+
+            Console.Write("Güncellenecek ürün Id :");
+            int productId = int.Parse(Console.ReadLine());
+
+            Console.Write("Güncellenecek ürün adı : ");
+            string productName = Console.ReadLine();
+
+            Console.Write("Güncellenecek ürün Fiyatı :");
+            decimal productPrice = decimal.Parse(Console.ReadLine());
+
+
+            SqlConnection connection = new SqlConnection("Data Source = MONSTER\\TEW_SQLEXPRESS; initial catalog =EgitimKampiDb; integrated security=true ");
+            connection.Open();
+            SqlCommand command = new SqlCommand("Update TblProduct Set ProductName=@productName,ProductPrice=@productPrice where ProductId=@productId", connection);
+            command.Parameters.AddWithValue("@productName", productName);
+            command.Parameters.AddWithValue("@productPrice", productPrice);
+            command.Parameters.AddWithValue("@productId", productId);
+
+            command.ExecuteNonQuery();
+
+            connection.Close();
+
+            Console.WriteLine("Güncelleme Başarılı");
+
+
             Console.Read();
+
+
+
+            #endregion
+
         }
     }
 }
